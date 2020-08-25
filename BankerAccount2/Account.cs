4865e4c7623e05fad4e341e6dab0c2fee00c1142
Andi Olle Olsen <zbcanols21@zbc.dk>
2020-08-25 20:28:49 +0200

    Add project files.

---
+++ BankerAccount2/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankerAccount2
+{
+    public class Account
+    {
+
+        public string Name { get; set; }
+        public int CardID { get; set; }
+        public int Balcence { get; set; }
+        public int Pin { get; set; }
+        
+        public Account(string name, int cardID, int balcence, int pin)
+        {
+            Name = name;
+            CardID = cardID;
+            Balcence = balcence;
+            Pin = pin;
+        }
+
+        public bool ValidateUser(int pin)
+        {
+            return (pin == this.Pin);
+        }
+    }
+}
---
+++ BankerAccount2/Withdraw.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankerAccount2
+{
+    public class WithDraw
+    {
+        public bool WithDrawMoney(double amount, double balance)
+        {
+            
+            if (balance < amount)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+    }
+}
